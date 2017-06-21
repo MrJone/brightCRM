ed5a191ee6e1d86a569efcbf0d4e10784154299a
MrJone <kukuruzkalol@yandex.ru>
2017-06-21 14:59:42 +0300

    Last commit from linux of 21/06/17

--- main.cpp
+++ main.cpp
@@ -7,7 +7,11 @@ static struct wMain wMainCfg;
 
 int main(int argc, char** argv)
 {
-    GtkWidget* mainWindow; //Основное окно
+    GtkWidget* mainWindow, //Основное окно
+        *btnView,
+        *btnEdit;
+
+    GtkWidget *fixed;
 
     if (cfg_get("bcrm.conf", &wMainCfg) != 0)
     {
@@ -26,13 +30,21 @@ int main(int argc, char** argv)
         wMainCfg.w,
         wMainCfg.h);
 
+    fixed = gtk_fixed_new();
+    gtk_container_add(GTK_CONTAINER(mainWindow),fixed);
+
+    btnEdit = gtk_button_new_with_label("Изменение данных");
+    gtk_fixed_put(GTK_FIXED(fixed), btnEdit, 0, 0);
+
+    //btnView = gtk_button_new_with_label("Просмотр данных");
+
     g_signal_connect(
         mainWindow,
         "destroy",
         G_CALLBACK(sig_destroy_mainWindow),
         NULL); //При закрытие окна вызываем sig_destroy_mainWindow()
 
-    gtk_widget_show(mainWindow);
+    gtk_widget_show_all(mainWindow);
 
     gtk_main();
 
